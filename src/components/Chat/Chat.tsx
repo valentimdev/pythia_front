ad0a07773d1444b5936b61724db6582418efa698
valentimdev <valentimdesenvolvedor@gmail.com>
2025-10-02 14:48:01 -0300

    feather image chat box

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import "./Chat.css";
 import TextInput from '../ChatText/TextInput.tsx';
 import balao from '@/assets/balao_duvida.svg'
+import feather from '@/assets/feather.svg'
+import ChatBox from '../ChatBox/ChatBox.tsx';
 function Chat() {
   return (
     <div className='chat_wrapper'> 
         <img className="question_ballon"src={balao}></img>
         <div className='chat_container'>
             <h1>RPG BOT</h1>
-            <TextInput />
+            <div className='chat_log'></div>
+            <div className="input_area">
+              <TextInput />
+              <img className="feather"src={feather}></img>
+              </div>
         </div>
     </div>
   )
---
+++ src/components/ChatBox/ChatBox.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChatBox() {
+  return (
+    <div className="chat_estructure">
+        <div className='chat_area'></div>
+    </div>
+  )
+}
+
+export default ChatBox
